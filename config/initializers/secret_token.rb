19d3967624a7994d3165e944079ba1c8fe963dec
Felipe Norato Lacerda <fnoratol@gmail.com>
2013-12-05 00:29:17 +0000

    Open House #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PolomarteApplication::Application.config.secret_key_base = '33e341e6df3e00f5a352a4bea954a22b255eaa24443f476f96de60b8acc881b8c930f9f432f4c6a42bab4b1e28abaceddf91c043fddfd97dcc72d0bb98b91cbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PolomarteApplication::Application.config.session_store :cookie_store, key: '_polomarte-application_session'
